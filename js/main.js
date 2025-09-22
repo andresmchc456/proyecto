0c35ddf3bfdb0128864fee6510531ef1f94c770a
Andrés Meléndez <andresmchc456@gmail.com>
2025-09-22 13:08:04 -0500

    feat: estructura de la pagina se realizo el home

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Función para cargar módulos HTML en un contenedor
+async function cargarModulo(url, contenedorId) {
+  const response = await fetch(url);
+  const html = await response.text();
+  document.getElementById(contenedorId).innerHTML = html;
+}
+
+// Llamadas para cargar los módulos
+// Cargar el módulo hola.html en el contenedor con id ''
+cargarModulo('vistas/home.html', 'home');
+cargarModulo('inc/barraNavegacion.html', 'barraNavegacion');
